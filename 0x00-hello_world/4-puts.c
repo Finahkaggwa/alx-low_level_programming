f40f9e9359efcbb3c3b47f934897df5eab94e996
Josephine Kaggwa <finahkaggwa@gmail.com>
2023-10-05 19:02:28 -0700

    Add 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Creating a C program that prints a sentence using the puts function
+ * Return: 0 (If the return is 0 then its successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
