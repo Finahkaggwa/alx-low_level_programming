dfb7a59294f9fb236da3b2307cd82fb45e19b0df
Josephine Kaggwa <finahkaggwa@gmail.com>
2023-10-05 19:15:52 -0700

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Creating a C program that prints a sentence using printf function
+ * Return: 0 (If the return is 0, then the code is sucessful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
