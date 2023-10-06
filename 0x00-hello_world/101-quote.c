c514c0843781acbb2ea5eedaf98fdaad59cb7509
Josephine Kaggwa <finahkaggwa@gmail.com>
2023-10-05 20:41:36 -0700

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.hd>
+/**
+ * main - Creating a C program that prints a sentence followed by a new line to the standard error
+ * Return: 1 (If the return is 1, then the code is successful)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
