49b59ab1ba90814072939371dfe4dc5ede3c6a84
Josephine Kaggwa <finahkaggwa@gmail.com>
2023-10-09 12:29:45 -0700

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return:0 should always return 0 as output
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
