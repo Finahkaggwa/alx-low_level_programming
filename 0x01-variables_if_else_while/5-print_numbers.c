ce273b9c8c273387664532138cb71da9bfcdc3ba
Josephine Kaggwa <finahkaggwa@gmail.com>
2023-10-09 13:44:34 -0700

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - It's the main function
+ * Return:0, should always be the return
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+		printf("%d", y);
+	printf("\n");
+	return (0);
+}
