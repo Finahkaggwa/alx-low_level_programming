04fac9809b73e3aa3845323c7d8e222ae346108d
Josephine Kaggwa <finahkaggwa@gmail.com>
2023-10-09 15:07:21 -0700

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - it's the main function
+ * Return:0 should always be
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
