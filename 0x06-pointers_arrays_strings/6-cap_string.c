d98d235df0789599e2de711aa6cff8d1775cd9dd
Josephine Kaggwa <finahkaggwa@gmail.com>
2023-10-19 10:58:56 -0700

    Modify 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,28 @@
 #include "main.h"
 /**
- * cap_string - capitalizes all words of a string
- * @str: parameter for the string to be capitalized
- * Return: pointer to the changed string
+ * cap_string - function that capitalizes all words of a string
+ * @str: parameter for input string
+ * Return: str which is the modified string
  */
 
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i, j;
+	char c[] = {44, 59, 46, '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
 
-	while (str[index])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '_' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				index == 0)
-			str[index] -= 32;
-		index++;
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (c[j] == str[i] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
 	}
 	return (str);
 }
