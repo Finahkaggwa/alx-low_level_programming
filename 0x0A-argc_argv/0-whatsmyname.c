811fec9c56fe606aad8e7ba5f3152ff1fda980e7
Josephine Kaggwa <finahkaggwa@gmail.com>
2023-10-30 02:23:21 -0700

    Add README.md, main.h and 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function that prints its name
+ * @argc: parameter for number of arguments
+ * @argv: parameter for array of arguments
+ * Return: 0 (Successful)
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
