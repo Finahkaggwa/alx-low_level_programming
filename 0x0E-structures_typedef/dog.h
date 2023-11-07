a819b73b059525040ef66c2b5d2aa07552661dbd
Josephine Kaggwa <finahkaggwa@gmail.com>
2023-11-06 18:05:03 -0800

    Add README.md, main.c files and dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - function for dog structure
+ * @name: parameter for dog name
+ * @age: parameter for dog age
+ * @owner: parameter for owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
